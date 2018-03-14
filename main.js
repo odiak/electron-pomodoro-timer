5eeb0c16e34a0919c31e0274c86f50cb0bf2efc4
Kaido Iwamoto <h6824.kaido@gmail.com>
2018-03-14 14:09:44 +0900

    Format

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const url = require("url");
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({ width: 400, height: 500 });
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    width: 400,
+    height: 500,
+    resizable: false,
+  });
   mainWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file",
-      slashes: true
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true,
     })
   );
 });
 
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   app.quit();
 });
